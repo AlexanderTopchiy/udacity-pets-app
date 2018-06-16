5c2a34551a340a884944f9f826e140b90bed864b
Alexander Topchiy <topchiyau@gmail.com>
2018-06-16 22:11:45 +0300

    Implement insertion pet from editor

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -36,6 +36,7 @@ import com.example.android.pets.data.PetDbHelper;
  */
 public class CatalogActivity extends AppCompatActivity {
 
+    /** Database helper that will provide us access to the database */
     private PetDbHelper mDbHelper;
 
     @Override
@@ -53,7 +54,14 @@ public class CatalogActivity extends AppCompatActivity {
             }
         });
 
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
         mDbHelper = new PetDbHelper(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
         displayDatabaseInfo();
     }
 
