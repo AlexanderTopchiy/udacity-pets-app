7cf7e017dfe05678c00577af390c5d0171bec5c1
Alexander Topchiy <topchiyau@gmail.com>
2018-06-16 13:05:39 +0300

    Update PetContract and CatalogActivity classes

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -2,26 +2,68 @@ package com.example.android.pets.data;
 
 import android.provider.BaseColumns;
 
+/**
+ * API Contract for the Pets app.
+ */
 public final class PetContract {
 
-
-    // Empty constructor is necessary
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
     private PetContract() {}
 
+    /**
+     * Inner class that defines constant values for the pets database table.
+     * Each entry in the table represents a single pet.
+     */
+    public static final class PetEntry implements BaseColumns {
 
-    // Create a database table and columns names
-    public static abstract class PetEntry implements BaseColumns {
+        /** Name of database table for pets */
+        public final static String TABLE_NAME = "pets";
 
-        public static final String TABLE_NAME = "pets";
+        /**
+         * Unique ID number for the pet (only for use in the database table).
+         *
+         * Type: INTEGER
+         */
+        public final static String _ID = BaseColumns._ID;
 
-        public static final String _ID = BaseColumns._ID;
-        public static final String COLUMN_NAME_NAME = "name";
-        public static final String COLUMN_NAME_BREED = "breed";
-        public static final String COLUMN_NAME_GENDER = "gender";
-        public static final String COLUMN_NAME_WEIGHT = "weight";
+        /**
+         * Name of the pet.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PET_NAME ="name";
 
+        /**
+         * Breed of the pet.
+         *
+         * Type: TEXT
+         */
+        public final static String COLUMN_PET_BREED = "breed";
+
+        /**
+         * Gender of the pet.
+         *
+         * The only possible values are {@link #GENDER_UNKNOWN}, {@link #GENDER_MALE},
+         * or {@link #GENDER_FEMALE}.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PET_GENDER = "gender";
+
+        /**
+         * Weight of the pet.
+         *
+         * Type: INTEGER
+         */
+        public final static String COLUMN_PET_WEIGHT = "weight";
+
+        /**
+         * Possible values for the gender of the pet.
+         */
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
     }
+
 }
